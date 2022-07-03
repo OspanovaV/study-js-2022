2bd97624dcb64c30e906d51c69b56ba8e5c18edd
OspanovaVita <vitaospanova@ukr.net>
2022-07-03 15:35:52 +0300

    Добавляет свойство innerHTML
    Свойство innerHTML: распарсит и создаст новые теги ;
     очищает быстро всё содержимое тега
    Вставка разметки с insertAdjacentHTML() -  добавит новую строку в разметку

--- js/08-templating.js
+++ js/08-templating.js
@@ -1,19 +1,19 @@
 /*
- * Свойство innerHTML
+ * Свойство innerHTML // возвращает всю вложенную разметку(весь контент) в виде одной строки
  * - чтение
  * - запись
  */
 
 const titleEl = document.querySelector('.title');
-// titleEl.innerHTML = '<a href="">Это ссылка)</a>';
-// titleEl.innerHTML = '';
+// titleEl.innerHTML = '<a href="">Это ссылка)</a>';//.innerHTML распарсит и создаст новые теги
+// titleEl.innerHTML = ''; //очищает быстро всё содержимое тега
 
 /*
  * Вставка разметки с insertAdjacentHTML()
  * http://fecore.net.ua/books/m5ph3r-javascript/module-07/dom-manipulation.html#%D0%BC%D0%B5%D1%82%D0%BE%D0%B4-insertadjacenthtml
  */
-
-titleEl.insertAdjacentHTML(
-  'beforeend',
-  '<a href="" class="title__link">Это ссылка)</a>',
+////добавить новую строку в разметку
+titleEl.insertAdjacentHTML( //передаём два значения:позиция, строка
+  'beforeend', //позиция(куда поставить)-в конце
+  '<a href="" class="title__link">Это ссылка)</a>', //что добавить(строку, которую хочу добавить)
 );
