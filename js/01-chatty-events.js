462bec416d48a4a336010b94bd713d516d5a7a75
OspanovaVita <vitaospanova@ukr.net>
2022-07-17 18:01:36 +0300

    Добавляет болтливые события
    -throttle - вызывает функцию вначале один раз, а затем вызывает её через указанный промежуток времени
    - debounce - вызывает функцию через указанный промежуток времени после окончания события

--- js/01-chatty-events.js
+++ js/01-chatty-events.js
@@ -5,15 +5,18 @@
 
 /*
  * Mousemove и throttle
+-throttle - вызывает функцию вначале один раз, а затем вызывает её через указанный промежуток времени
  */
 const coordsOutputRef = document.querySelector('.js-coords');
-let mouseMoveCbInvocationCounter = 0;
+let mouseMoveCbInvocationCounter = 0;//переменная хранит количество вызовов функции
 
-window.addEventListener('mousemove', _.throttle(onMouseMove, 250));
+//на окно добавляем слушателя событий 'mousemove'
+window.addEventListener('mousemove', _.throttle(onMouseMove, 250));//вызываем функцию не чаще чем 250ms(метод  _.throttle)
 
 function onMouseMove(event) {
   mouseMoveCbInvocationCounter += 1;
 
+  //записываем количество вызовов и координаты мышки на экране
   coordsOutputRef.textContent = `
     Кол-во вызовов onMouseMove: ${mouseMoveCbInvocationCounter},
     X: ${event.clientX},
@@ -22,17 +25,19 @@ function onMouseMove(event) {
 }
 
 /*
- * Input и debounce
+ * Input и debounce (отложенный поиск)
+- debounce - вызывает функцию через указанный промежуток времени после окончания события
  */
 const inputRef = document.querySelector('.js-input');
 const outputRef = document.querySelector('.js-output');
 let inputCbInvocationCounter = 0;
 
-inputRef.addEventListener('input', _.debounce(onInputChange, 300));
+inputRef.addEventListener('input', _.debounce(onInputChange, 300));//вызываем функцию при событии 'input'через 300ms( _.debounce)
 
 function onInputChange(event) {
   inputCbInvocationCounter += 1;
 
+  //выводим количество вызовов этой функции и значение .target(введённые в инпут символы)
   outputRef.textContent = `
     Кол-во вызовов onInputChange: ${inputCbInvocationCounter},
     Значение: ${event.target.value}
