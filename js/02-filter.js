cc90f40f9eb0b83946bc6d036af022ed67cbeef9
OspanovaVita <vitaospanova@ukr.net>
2022-07-17 18:46:55 +0300

    Добавляет фильтр с поиском

--- js/02-filter.js
+++ js/02-filter.js
@@ -15,6 +15,7 @@ const tech = [
 ];
 
 /*
+*Делаем фильтр с поиском
  * 1. Рендерим разметку элементов списка
  * 2. Слушаем изменение фильтра
  * 3. Фильтруем данные и рендерим новые элементы
@@ -25,27 +26,30 @@ const refs = {
   input: document.querySelector('#filter'),
 };
 
-refs.input.addEventListener('input', _.debounce(onFilterChange, 300));
+refs.input.addEventListener('input', _.debounce(onFilterChange, 300));//вызываем функцию при событии 'input'через 300ms( _.debounce)
 
 const listItemsMarkup = createListItemsMarkup(tech);
 populateList(listItemsMarkup);
 
+//Рендерим разметку элементов списка(для того чтобы пользователь увител список)
 function createListItemsMarkup(items) {
   return items.map(item => `<li>${item.label}</li>`).join('');
 }
-
+//обработчик события
 function onFilterChange(evt) {
-  console.log('INPUT');
   const filter = evt.target.value.toLowerCase();
 
-  const filteredItems = tech.filter(t =>
-    t.label.toLowerCase().includes(filter),
+  //фильтруем те элементы, которые содержат значение фильтра в свойстве .label
+  const filteredItems = tech.filter(t => //берем весь массив данных(если у каждого элемента в нижнем регистре
+    t.label.toLowerCase().includes(filter), //включают в себя значение фильтра(.includes(filter)) в свойстве .label)
   );
 
+  //создаём новую разметку отфильтрованных элементов  
   const listItemsMarkup = createListItemsMarkup(filteredItems);
-  populateList(listItemsMarkup);
+  populateList(listItemsMarkup);//и полностью заменяем значение списка
 }
 
+//функция для замены значений списка
 function populateList(markup) {
   refs.list.innerHTML = markup;
 }
